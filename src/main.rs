c093454816a9c55f9862fd868c02534063d2131b
ST4LTH <66903577+ST4LTH@users.noreply.github.com>
2025-03-03 16:46:27 +0100

    api

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod models;
 mod components;
 
-use std::fs;
 use serde_json;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use models::server::{Servers, ResourceCountsType};
+use std::fs;
+use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
-use std::time::{Instant}; 
+use std::time::Instant; 
 use humantime::format_duration;
 use components::fetcher::Fetcher;
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -36,5 +40,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let elapsed_time = start_time.elapsed(); 
     println!("Time taken to fetch and print top 10 servers: {}", format_duration(elapsed_time));
 
+    let resource_counts_shared = Arc::new(Mutex::new(resource_counts));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(resource_counts_shared.clone()))
+            .route("/top-resources", web::get().to(get_top_resources))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await?;
+
     Ok(())
+}
+
+async fn get_top_resources(
+    resource_counts: web::Data<Arc<Mutex<HashMap<String, ResourceCountsType>>>>,
+) -> impl Responder {
+    let resource_counts = resource_counts.lock().unwrap();
+
+    let mut resource_vec: Vec<(&String, &ResourceCountsType)> = resource_counts.iter().collect();
+    resource_vec.sort_by(|a, b| b.1.servers.cmp(&a.1.servers));
+
+    let top_resources: Vec<_> = resource_vec
+        .iter()
+        .take(10)
+        .map(|(resource, counts)| {
+            serde_json::json!({
+                "resource": resource,
+                "servers": counts.servers,
+                "players": counts.players,
+            })
+        })
+        .collect();
+
+    HttpResponse::Ok().json(top_resources)
 }
